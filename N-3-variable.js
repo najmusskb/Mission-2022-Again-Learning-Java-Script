c89e515d9dec9752fcf6873e855c22e5a3a520c3
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 01:00:56 +0600

    var declare

---
+++ N-3-variable.js
@@ -0,0 +1,19 @@
+// vary + Able= variable ;
+
+/* 
+ variable declare korte mot 5 ta jinish lage (VVI)
+
+
+ 1. var 
+
+
+
+*/
+
+
+
+var price = 45;
+var age = 25;
+var temperature = 38;
+var weight = 40;
+var address = 220;
