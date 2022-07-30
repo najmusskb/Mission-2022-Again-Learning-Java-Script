2ad6fe73ccd4125e244300985bf47c4edb26022e
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 00:46:41 +0600

    math preparation and some instraction

---
+++ N-2-math.js
@@ -0,0 +1,30 @@
+/* 
+
+
+1.interger number, odd , even  = 5,10,20
+
+2.float = 2.36, 56.32 ;
+
+3.operation = additon ,subtraction ,division,multiplication, sumation ETC
+4.percentage    ,
+5.average
+6.comarision : >, <,  == , (soman na   !=, >=, <=)
+
+
+7. OIKIK niyom = 2k apple and 5k angur 
+8. 300,600,54,45 = max , min ,avg , sum , 
+9.  series problem  = 
+
+extra 
+
+1.prime number ? 
+2.permutation ?
+3.area of a triangle
+
+:::::::Allways:::::: 
+jodi lage 
+_____________
+
+
+
+*/
