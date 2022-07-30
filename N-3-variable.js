6f759fbb2a563653627f788afd8755fe2eebd4d3
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 01:14:18 +0600

    variable type numeric , string, boolean

--- N-3-variable.js
+++ N-3-variable.js
@@ -10,10 +10,45 @@
 
 */
 
-
+// numeric
 
 var price = 45;
 var age = 25;
 var temperature = 38;
 var weight = 40;
 var address = 220;
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+variable type , numeric , String , Boolean 
+*/
+
+
+// String 
+var person = "najmus sakib";
+
+var location = "Dhaka mirput Sewrapara";
+
+var specialPerson = "alia Bhat";
+
+
+
+
+// boolean ( 0 and 1     0 = false , 1 = true or sotto = true , false = mittha  )
+
+
+
+var serious = true;
+var fullMarks = true;
+
+var areYouSingle = false;
