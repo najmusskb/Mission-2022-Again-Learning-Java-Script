530e1e91f694c82eb3b5e49dac1fe607ba738f57
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 20:06:40 +0600

    remainder

--- N-12-practice-problem-02.js
+++ N-12-practice-problem-02.js
@@ -12,8 +12,18 @@ var TotalMarks = mathemetices + biology + chemistry + physics + bangla;
 //  get avgrage value 
 
 var AvarageMarks = TotalMarks / 5;
-
 AvarageMarks = AvarageMarks.toFixed(2);
 AvarageMarks = parseFloat(AvarageMarks);
 
 console.log(AvarageMarks);
+
+
+
+
+// pracrtice problem 
+
+
+var input = 119;
+var input2 = 5;
+var output = input % input2;
+console.log(output);
