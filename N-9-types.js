964813d02d22e814b2aa70dab288982ee72bc880
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 19:36:45 +0600

    typeof tofixed

---
+++ N-9-types.js
@@ -0,0 +1,38 @@
+var price = '30';
+console.log(typeof price);
+
+
+
+
+
+var price = 30;
+console.log(typeof price);
+
+
+
+var IsHAppy = false;
+console.log(typeof IsHAppy);
+
+
+
+
+
+
+
+
+var Romantic;
+console.log(typeof Romantic);
+
+
+
+
+
+
+// Tofixed
+
+var Number1 = 0.1;
+var Number2 = 0.2;
+var Total = Number1 + Number2;
+Total = Total.toFixed(5);
+Total = parseFloat(Total);
+console.log(Total);
