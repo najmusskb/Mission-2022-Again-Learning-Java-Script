a28c9f6d22996ff1a4c7438ea6836a883d3336f3
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 19:43:21 +0600

    remainder

---
+++ N-10-remainder.js
@@ -0,0 +1,18 @@
+
+//      / this means vag fol
+//      % this means vag sesh 
+
+var mangoes = 11;
+var person = 2;
+//    / this means vag fol
+
+console.log(mangoes / person);
+
+
+
+
+
+var mangoes = 11;
+var person = 3;
+//      % this means vag sesh 
+console.log(mangoes % person);
