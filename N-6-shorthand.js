8adbb9a09b20381c874649990fb2d0166f544c57
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 13:52:42 +0600

    shorthand

---
+++ N-6-shorthand.js
@@ -0,0 +1,35 @@
+// variable decleration
+
+
+
+
+// change valu and update 
+/* var price = 30;
+var price2 = 99;
+price = price + 20;
+console.log(price); */
+
+
+
+// addition to the same variable shorthand
+
+// += adding some value to the existing value
+var price2 = 99;
+price2 += 20;
+console.log("addition:", price2)
+
+
+var price2 = 99;
+price2 -= 20;
+console.log("subtraction:", price2)
+
+
+var price2 = 99;
+price2 *= 20;
+console.log("multiplication:", price2)
+
+
+
+var price2 = 99;
+price2 /= 20;
+console.log("division", price2)
