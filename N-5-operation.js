417f016032bcccbb75a9c1a06658f00ef732adbf
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-07-31 13:19:38 +0600

    sumation subtraction multiplication division

---
+++ N-5-operation.js
@@ -0,0 +1,43 @@
+/* var onionPrice = 20;
+var eggPrice = 45;
+console.log(onionPrice + eggPrice);
+
+ */
+
+
+
+
+/* // difference
+var onionPrice = 20;
+var eggPrice = 45;
+var TotalPrice = onionPrice + eggPrice;
+
+// console.log(TotalPrice);
+// subtraction
+var difference = onionPrice - eggPrice;
+console.log(difference);
+ */
+
+
+
+
+// quantity Multiplication
+
+/* var orangePrice = 20;
+var quantity = 7;
+
+var TotalCost = orangePrice * quantity;
+
+console.log(TotalCost);
+ */
+
+
+
+// division 
+
+var money = 500;
+var player = 10;
+
+var PlayerGotMoney = money / player;
+
+console.log(PlayerGotMoney);
