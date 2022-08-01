81354006235c2938cd738ae41e3968022661b013
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-01 15:30:30 +0600

    push pop and comarism

---
+++ N-17-push-pop.js
@@ -0,0 +1,47 @@
+/* //  push example for number
+
+//  push ...push means push kore dhukano 
+
+var numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+console.log(numbers);
+
+
+
+// SOBAR SESH E ARRAY 63 JOG HOBE 
+numbers.push(63);
+console.log(numbers);
+
+
+
+//  anouther example for string 
+
+var friends = ['rahim', 'karim', 'jasim'];
+
+friends.push('sakib');
+
+console.log(friends);
+ */
+
+
+
+
+
+
+//  use pop to get last element 
+
+
+var friends = ['rahim', 'karim', 'jasim'];
+
+friends.pop();
+console.log(friends);
+
+
+
+
+
+
+var numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
---
+++ N-18-comparism.js
@@ -0,0 +1,39 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(5 != 6);
+// console.log(5 <= 6);
+// console.log(15 >= 6);
+
+
+
+// and option
+
+/* var money1 = 10000;
+var money2 = 200;
+var result1 = 6000;
+var result2 = 300;
+var height1 = 200;
+var height2 = 30;
+
+var totalresult = money1 > money2 && result1 > result2 && height1 > height2;
+
+
+
+console.log(totalresult);
+ */
+
+
+
+// or option
+
+var money1 = 10000;
+var money2 = 200;
+var result1 = 6000;
+var result2 = 300;
+var height1 = 200;
+var height2 = 30;
+
+var totalresult = money1 > money2 || result1 < result2 || height1 < height2;
+
+console.log(totalresult);
