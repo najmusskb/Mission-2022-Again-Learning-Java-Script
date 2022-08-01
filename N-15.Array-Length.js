c4037271d5a01c5ff1c3b8841100209dd665429d
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-01 12:21:28 +0600

    set element in a array , index position,value

---
+++ N-15.Array-Length.js
@@ -0,0 +1,34 @@
+//  Array Declaretion 
+
+var FriendsAge = [14, 25, 36, 14, 25, 32, 12];
+
+var picnic = [78, 98, 52, 1, 56, 48, 41, 57, 1, 25, 4, 45];
+
+console.log(FriendsAge.length);
+console.log(picnic.length);
+
+
+
+//  string 
+
+
+var naika = [
+
+    'taniya',
+    'sadiya',
+    'madiya',
+    'mim',
+    'afsana',
+    'bithi',
+    'oishi',
+    'kanij',
+    'aliya batt',
+    'sabnur',
+    'miya',
+    'sunny',
+    'natasha',
+    'dilruba'
+];
+
+
+console.log(naika.length);
---
+++ N-16-Array-index.js
@@ -0,0 +1,35 @@
+//find index element form a Array
+
+
+var numbers = [12, 131, 20, 89, 36, 7, 15, 89, 45, 36, 478, 99, 5, 2, 0, 69, 87, 8];
+
+
+
+
+
+// get element by index
+
+var elements = numbers[1];
+console.log(elements);
+
+
+
+
+
+//  change array numbers or set elements value by index;
+numbers[1] = 77;
+numbers[5] = 100;
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+//  find index of an elements (vvi)
+
+var positionIndex = numbers.indexOf(89)
+console.log(positionIndex);
