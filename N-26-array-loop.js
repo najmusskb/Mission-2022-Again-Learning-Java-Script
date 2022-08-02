963da5a1f75285812f61088b30936caedda53f8f
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-03 00:53:22 +0600

    string array with for loop

--- N-26-array-loop.js
+++ N-26-array-loop.js
@@ -7,10 +7,18 @@
 
 
 
-var numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 2, 5, 47];
+/* var numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 2, 5, 47];
 
 for (var i = 0; i <= numbers.length; i++) {
 
     var number = numbers[i]
     console.log(number);
+} */
+
+
+
+var items = ['kola', 'mola', 'jola', 'sola', 'pola']
+for (var i = 0; i <= items.length; i++) {
+    var item = items[i];
+    console.log(item);
 }
