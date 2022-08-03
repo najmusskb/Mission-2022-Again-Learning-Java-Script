01ae7e4f931be29641acc2bdaf1fd5b0ba5af230
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-03 10:42:26 +0600

    break continue

---
+++ N-27-break.js
@@ -0,0 +1,51 @@
+/* for (var i = 0; i < 20; i++) {
+
+
+    console.log(i);
+
+    if (i > 15) {
+        break;
+    }
+} */
+
+
+
+
+
+/* 
+var names = ['sakib', 'rakib', "makib", "jakib", 'fakib'];
+for (var i = 0; i <= names.length; i++) {
+
+
+    var Name = names[i];
+    if (Name == 'jakib') {
+        break
+    }
+    console.log(Name);
+}
+ */
+
+
+
+
+
+var numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 2, 30, 78, 59, 63, 15]
+
+for (var i = 0; i <= numbers.length; i++) {
+
+    var itemsNumber = numbers[i];
+
+    if (itemsNumber > 70) {
+
+        break;
+    }
+    console.log(itemsNumber);
+}
+
+
+
+
+
+
+
+
---
+++ N-28-continue.js
@@ -0,0 +1,19 @@
+// continue mane hocche skip kora 
+
+
+var numbers = [1, 2, 10, 20, 30, 40, 50, 60, 26, 70, 80];
+
+for (var i = 0; i < numbers.length; i++) {
+
+    var Number = numbers[i];
+
+    if (Number > 50) {
+
+        continue;
+
+    }
+    console.log(Number);
+
+}
+
+
