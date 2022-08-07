bbfdc916850c2fa943d9369d2a9413ea01ca17ab
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-07 21:16:41 +0600

    sowap solving

---
+++ N-56-Swap-variable-swap-without-temp-destructing.js
@@ -0,0 +1,35 @@
+// 21-5 Swap variable, swap without temp, destructing (VVI)
+
+
+let A = 10;
+let B = 23;
+console.log('Problem ', A, B);
+let temp = 0;
+temp = A;
+A = B;
+B = temp;
+console.log('Sowap Probel ', A, B);
+
+
+
+
+//  Anouther way destructing for sowap
+
+A1 = 25;
+B1 = 20;
+[A1, B1] = [B1, A1];
+console.log(A1, B1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
