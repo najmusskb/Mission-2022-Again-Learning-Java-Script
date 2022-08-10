9695ef788e8fd61b0d970ee09e78903bc2083d79
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-10 19:59:09 +0600

    67 to 70

---
+++ N-67-feet-to-inch.js
@@ -0,0 +1,13 @@
+function feetToInch(feet) {
+
+    const inchValue = 12;
+    const feetinput = feet;
+    const inch = feetinput * inchValue;
+    return inch;
+}
+
+const feetToIjnchValue = 28;
+
+const result = feetToInch(feetToIjnchValue);
+
+console.log('feet to inch result : ', result);
---
+++ N-68-centemeter-to-metter.js
@@ -0,0 +1,14 @@
+// cm to m convert
+
+function centimeterToMeter(cm) {
+
+    const dividelengthvalue = 100;
+    const result = cm / 100;
+    return result;
+}
+
+
+const Cminput = 8;
+
+const Finalresult = centimeterToMeter(Cminput);
+console.log(Finalresult);
