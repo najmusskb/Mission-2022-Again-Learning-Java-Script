add682c111268d20f5464bdee2380a18e1e16ea6
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-12 22:31:25 +0600

    factorial

---
+++ N-71-simple-recursion.js
@@ -0,0 +1,97 @@
+/* let sum = 0;
+for (let i = 1; i <= 5; i++); {
+
+    sum = sum + i;
+
+}
+console.log(sum); */
+/* 
+let sum = 0;
+for (let i = 5; i >= 1; i--); {
+
+    sum = sum + i;
+
+}
+console.log(sum);
+
+
+ */
+
+
+
+/* function sum(i) {
+
+    // stoping condition
+    if (i == 1) {
+        return 1;
+    }
+    // console.log(i);
+
+    //  sum(i-1 ) call kora 
+    return i + sum(i - 1);
+}
+
+const result = sum(5);
+
+console.log(result);
+ */
+
+
+
+// factoriyal 
+
+
+let factorial = 1;
+for (let i = 5; i >= 1; i--) {
+
+    factorial = factorial * i;
+}
+
+console.log(factorial);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// factoriyal 
+/* 
+function factoriyall() {
+
+
+    if (i == 1) {
+
+        return 1;
+    }
+
+    let factoriyal = 1;
+    for (let i = 5; i >= 1; i++) {
+
+
+        factoriyal = factoriyal * i;
+    }
+}
+
+
+
+const result = factoriyall(5);
+
+console.log(result);
+
+
+ */
+
+
+
+
+
---
+++ N-72-factoriyal.js
@@ -0,0 +1,23 @@
+// factoriyal 
+
+
+/* let factorial = 1;
+for (let i = 5; i >= 1; i--) {
+
+    factorial = factorial * i;
+}
+
+console.log(factorial); */
+
+
+
+
+function factoriall(i) {
+    if (i == 1) {
+        return 1;
+    }
+    return i * factoriall(i - 1);
+}
+const result = factoriall(5);
+console.log(result);
+
